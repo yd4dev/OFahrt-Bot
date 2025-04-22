670c0b23d0edb9b4fa2c64491b1128c5a0be6156
Dennis Yildirim <dennis@yld.im>
2025-04-22 18:11:30 +0200

    Initial Commit

---
+++ src/main/java/bot/Main.java
@@ -0,0 +1,23 @@
+package tuda.ofahrt;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        String token = dotenv.get("token");
+
+        JDA bot = JDABuilder.create(token, List.of(GatewayIntent.values()))
+                .addEventListeners(new MyListener())
+                .build();
+
+
+    }
+}
